9d65a9e7941776c1b3dff170836446a4720e98d8
Khaled <a.khaledshehab@gmail.com>
2023-02-10 19:01:35 +0200

    finished chat screen

--- lib/screens/chat_screen.dart
+++ lib/screens/chat_screen.dart
@@ -4,6 +4,8 @@ import 'package:chat_app/widgets/widgets.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 
+import '../widgets/message_tile.dart';
+
 class ChatScreen extends StatefulWidget {
   final String userName, groupId, groupName;
   const ChatScreen(
@@ -18,6 +20,7 @@ class ChatScreen extends StatefulWidget {
 
 class _ChatScreenState extends State<ChatScreen> {
   Stream<QuerySnapshot>? chats;
+  TextEditingController messageController = TextEditingController();
   String admin = "";
 
   @override
@@ -59,6 +62,83 @@ class _ChatScreenState extends State<ChatScreen> {
               icon: const Icon(Icons.info))
         ],
       ),
+      body: Stack(children: <Widget>[
+        chatMessages(),
+        Container(
+          alignment: Alignment.bottomCenter,
+          width: MediaQuery.of(context).size.width,
+          child: Container(
+            padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 18),
+            width: MediaQuery.of(context).size.width,
+            color: Colors.grey[700],
+            child: Row(children: [
+              Expanded(
+                  child: TextFormField(
+                controller: messageController,
+                style: const TextStyle(color: Colors.white),
+                decoration: const InputDecoration(
+                    hintText: "Send a message...",
+                    hintStyle: TextStyle(color: Colors.white, fontSize: 16),
+                    border: InputBorder.none),
+              )),
+              const SizedBox(
+                width: 12,
+              ),
+              GestureDetector(
+                onTap: () {
+                  sendMessage();
+                },
+                child: Container(
+                  height: 50,
+                  width: 50,
+                  decoration: BoxDecoration(
+                    color: Theme.of(context).primaryColor,
+                    borderRadius: BorderRadius.circular(30),
+                  ),
+                  child: const Center(
+                      child: Icon(
+                    Icons.send,
+                    color: Colors.white,
+                  )),
+                ),
+              )
+            ]),
+          ),
+        ),
+      ]),
     );
+  }
+
+  chatMessages() {
+    return StreamBuilder(
+        stream: chats,
+        builder: ((context, AsyncSnapshot snapshot) {
+          return snapshot.hasData
+              ? ListView.builder(
+                  itemCount: snapshot.data.docs.length,
+                  itemBuilder: (context, index) {
+                    return MessageTile(
+                      message: snapshot.data.docs[index]['message'],
+                      sender: snapshot.data.docs[index]['sender'],
+                      sentByMe: widget.userName ==
+                          snapshot.data.docs[index]['sender'],
+                    );
+                  })
+              : Container();
+        }));
+  }
+
+  sendMessage() {
+    if (messageController.text.isNotEmpty) {
+      Map<String, dynamic> chatMessageMap = {
+        "message": messageController.text,
+        "sender": widget.userName,
+        "time": DateTime.now().millisecondsSinceEpoch
+      };
+      DatabaseService().sendMessage(widget.groupId, chatMessageMap);
+      setState(() {
+        messageController.clear();
+      });
+    }
   }
 }
--- lib/service/database_service.dart
+++ lib/service/database_service.dart
@@ -122,4 +122,14 @@ class DatabaseService {
       });
     }
   }
+
+// Send a Message
+  sendMessage(String groupId, Map<String, dynamic> chatMessageData) async {
+    groupCollection.doc(groupId).collection("messages").add(chatMessageData);
+    groupCollection.doc(groupId).update({
+      "recentMessage": chatMessageData['message'],
+      "recentMessageSender": chatMessageData['sender'],
+      "recentMessageTime": chatMessageData['time'].toString(),
+    });
+  }
 }
